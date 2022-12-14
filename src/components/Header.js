b7bfd9bff04bf9ac99cce2afb06e75820ec2d127
Marcos Adriano <marcosadriano.ti@gmail.com>
2022-12-14 14:59:30 -0300

    Requisito 5

--- src/components/Header.js
+++ src/components/Header.js
@@ -31,24 +31,24 @@ class Header extends React.Component {
         { isLoading && <Loading /> }
         <h1 data-testid="header-user-name">
           { userName }
-          <ul>
-            <li>
-              <Link to="/search" data-testid="link-to-search">
-                Search
-              </Link>
-            </li>
-            <li>
-              <Link to="/favorites" data-testid="link-to-favorites">
-                Favorites
-              </Link>
-            </li>
-            <li>
-              <Link to="/profile" data-testid="link-to-profile">
-                Profile
-              </Link>
-            </li>
-          </ul>
         </h1>
+        <ul>
+          <li>
+            <Link to="/search" data-testid="link-to-search">
+              Search
+            </Link>
+          </li>
+          <li>
+            <Link to="/favorites" data-testid="link-to-favorites">
+              Favorites
+            </Link>
+          </li>
+          <li>
+            <Link to="/profile" data-testid="link-to-profile">
+              Profile
+            </Link>
+          </li>
+        </ul>
       </header>
     );
   }
--- src/pages/Search.js
+++ src/pages/Search.js
@@ -2,10 +2,38 @@ import React from 'react';
 import Header from '../components/Header';
 
 class Search extends React.Component {
+  state = {
+    // artistName: '',
+    artistNameLength: false,
+  };
+
+  validationArtist = (e) => {
+    const lengthMin = 2;
+    this.setState({
+      // artistName: e.target.value,
+      artistNameLength: e.target.value.length >= lengthMin,
+    });
+  };
+
   render() {
+    const { artistNameLength } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
+        <input
+          type="text"
+          name="artist-name"
+          data-testid="search-artist-input"
+          onChange={ this.validationArtist }
+        />
+        <button
+          type="button"
+          data-testid="search-artist-button"
+          disabled={ !artistNameLength }
+          // onClick={}
+        >
+          Pesquisar
+        </button>
       </div>
     );
   }
