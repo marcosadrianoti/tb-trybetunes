6c83a100031da20c0e22b61f4c2dfd4628bc18c7
Marcos Adriano <marcosadriano.ti@gmail.com>
2022-12-14 10:35:00 -0300

    Requisito 2

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 import Login from './pages/Login';
 import Search from './pages/Search';
 import Album from './pages/Album';
@@ -7,19 +7,64 @@ import Favorites from './pages/Favorites';
 import Profile from './pages/Profile';
 import ProfileEdit from './pages/ProfileEdit';
 import NotFound from './pages/NotFound';
+import { createUser } from './services/userAPI';
 
 class App extends React.Component {
+  state = {
+    userName: '',
+    userNameLength: false,
+    isLoading: false,
+    isLoged: false,
+  };
+
+  validationUser = (e) => {
+    const lengthMin = 3;
+    this.setState({
+      userName: e.target.value,
+      userNameLength: e.target.value.length >= lengthMin,
+    });
+  };
+
+  savingUser = async (userName) => {
+    this.setState({
+      isLoading: true,
+    });
+    await createUser({ name: `${userName}` });
+    this.setState({
+      isLoading: false,
+      isLoged: true,
+    });
+  };
+
   render() {
+    const { userNameLength, userName, isLoading, isLoged } = this.state;
     return (
       <BrowserRouter>
         <p>TrybeTunes...</p>
-        <Route path="/search" component={ Search } />
-        <Route path="/album/:id" component={ Album } />
-        <Route path="/favorites" component={ Favorites } />
-        <Route path="/profile/edit" component={ ProfileEdit } />
-        <Route path="/profile" component={ Profile } />
-        <Route path="/" component={ Login } />
-        <Route path="*" component={ NotFound } />
+        <Switch>
+          <Route path="/search" component={ Search } />
+          <Route path="/album/:id" component={ Album } />
+          <Route path="/favorites" component={ Favorites } />
+          <Route path="/profile/edit" component={ ProfileEdit } />
+          <Route path="/profile" component={ Profile } />
+          <Route
+            path="/"
+            render={ () => (
+              isLoged
+                ? <Redirect to="/search" />
+                : (
+                  <Login
+                    userName={ userName }
+                    userNameLength={ userNameLength }
+                    isLoading={ isLoading }
+                    validationUser={ this.validationUser }
+                    savingUser={ this.savingUser }
+                  />
+                )
+            ) }
+          />
+          <Route path="*" component={ NotFound } />
+        </Switch>
       </BrowserRouter>
     );
   }
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,11 +1,54 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import Loading from '../components/Loading';
 
 class Login extends React.Component {
   render() {
+    const {
+      validationUser,
+      userNameLength,
+      userName,
+      isLoading,
+      savingUser,
+    } = this.props;
     return (
-      <div data-testid="page-login" />
+      <div data-testid="page-login">
+        {
+          (
+            isLoading
+              ? <Loading />
+              : (
+                <label htmlFor="user-name">
+                  Nome
+                  <input
+                    type="text"
+                    name="user-name"
+                    data-testid="login-name-input"
+                    onChange={ validationUser }
+                  />
+                  <button
+                    type="button"
+                    data-testid="login-submit-button"
+                    disabled={ !userNameLength }
+                    onClick={ () => (savingUser(userName)) }
+                  >
+                    Entrar
+                  </button>
+                </label>
+              )
+          )
+        }
+      </div>
     );
   }
 }
+
+Login.propTypes = {
+  validationUser: PropTypes.func.isRequired,
+  savingUser: PropTypes.func.isRequired,
+  userNameLength: PropTypes.bool.isRequired,
+  isLoading: PropTypes.bool.isRequired,
+  userName: PropTypes.string.isRequired,
+};
 
 export default Login;
