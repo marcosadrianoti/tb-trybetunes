8b361a53c4b2ec4ea992284dc967f2df3c6e9bf6
Marcos Adriano <marcosadriano.ti@gmail.com>
2022-12-21 17:24:31 -0300

    Requisito 13 - Bonus

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,13 +1,44 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
+import { getUser } from '../services/userAPI';
 
 class Profile extends React.Component {
+  state = {
+    name: '',
+    email: '',
+    image: '',
+    description: '',
+  };
+
+  componentDidMount() {
+    this.userProfile();
+  }
+
+  userProfile = async () => {
+    this.setState({
+    });
+    const user = await getUser();
+    this.setState({
+      name: user.name,
+      email: user.email,
+      image: user.image,
+      description: user.description,
+    });
+  };
+
   render() {
-    return (
+    const { name, email, image, description } = this.state;
+    return ((
       <div data-testid="page-profile">
         <Header />
+        <img src={ image } alt={ name } data-testid="profile-image" />
+        <p>{ name }</p>
+        <p>{ email }</p>
+        <p>{ description }</p>
+        <Link to="/profile/edit">Editar perfil</Link>
       </div>
-    );
+    ));
   }
 }
 
